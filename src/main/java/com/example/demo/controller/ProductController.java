88dc5dbf2d5b4d8f3cdcbf933d017f90218e211e
dinh viet <dinhv2388@gmail.com>
2020-10-23 10:04:39 +0700

    add springboot crud

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,71 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Category;
+import com.example.demo.model.Product;
+import com.example.demo.service.category.CategoryService;
+import com.example.demo.service.product.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @ModelAttribute("categories")
+    public Iterable<Category> categories(){
+        return categoryService.findAll();
+    }
+
+    @GetMapping("")
+    public String getIndex(Model model){
+        model.addAttribute("products",productService.findAll());
+        return "index";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model){
+        model.addAttribute("product", new Product());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(Product product){
+        productService.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id){
+        Optional<Product> product = productService.findById(id);
+        if (product.isPresent()){
+            productService.remove(id);
+        }else
+            return "notfound";
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model){
+        Optional<Product> product = productService.findById(id);
+        model.addAttribute("product",product);
+        return "edit";
+    }
+
+
+    @PostMapping("/edit")
+    public String editProduct(Product product){
+        productService.save(product);
+        return "redirect:/products";
+    }
+}
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    @ManyToOne
+    private Category category;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
